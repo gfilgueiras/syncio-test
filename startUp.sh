e76d2ce92b7e5a9437421ec3d8b11426f46f8b1e
Gustavo Filgueiras <gfilgueirasrj@gmail.com>
2025-10-17 18:55:59 -0300

    Add // Script // Has been added a script to strat up the project.

---
+++ startUp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker compose build --no-cache; docker compose up -d ;
+docker exec -w /var/www/syncio/backend sync_php composer install;
+docker exec -w /var/www/syncio/backend sync_php cp .env.example .env;
+docker exec -w /var/www/syncio/backend sync_php php artisan key:generate;
+docker exec sync_node sh -c "cd /app && npm install && npm run build";
+docker compose restart nginx;
+docker exec -d sync_node sh -c "cd /app && npm run dev -- --host 0.0.0.0";
+docker exec -w /var/www/syncio/backend sync_php php artisan migrate;
